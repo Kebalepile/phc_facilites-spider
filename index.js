439dd28f2edf3ab84c250a962ca47ac9e1d01222
Kebalepile <pimpabutterfly86@gmail.com>
2023-06-05 14:51:47 +0200

    helath facilities methods.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { launch } from "puppeteer";
 import { HealthEstablishmentsSpider } from "./spiders/healthEstablishments.js";
 async function crawler() {
-  const browser = await launch({ headless: false });
+  const browser = await launch({ headless: "new" });
 
   const HESpider = new HealthEstablishmentsSpider(browser);
   await HESpider.startSpider();
--- spiders/healthEstablishments.js
+++ spiders/healthEstablishments.js
@@ -1,9 +1,8 @@
-import { toJson} from "../pipline/healthEstablishmentPipline.js";
-
+import { toJson } from "../pipline/healthEstablishmentPipline.js";
 
 export class HealthEstablishmentsSpider {
   constructor(browser) {
-    this.name = "Health Establlishment"
+    this.name = "Health Establlishment";
     this.page = null;
     this.browser = browser;
     this.allowedDomains = [
@@ -16,7 +15,7 @@ export class HealthEstablishmentsSpider {
       if (!this.page) {
         await this.setPage();
       }
-      console.log(`${this.name} spider, has began crawling for data.`)
+      console.log(`${this.name} spider, has began crawling for data.`);
       this.provincesData();
     } catch (error) {
       consle.log(error);
@@ -32,6 +31,8 @@ export class HealthEstablishmentsSpider {
 
   async provincesData() {
     try {
+      console.log("Getting province data.");
+
       await this.page.goto(this.allowedDomains[0]);
       await this.page.waitForSelector("body");
       let optionValues = await this.page.$$eval("#province option", (options) =>
@@ -44,20 +45,18 @@ export class HealthEstablishmentsSpider {
       for (let i = 0; i <= optionValues.length; i++) {
         let { done, value } = iterator.next();
         if (done) {
-          console.log("done");
-          // console.log(this.data);
           toJson(this.data);
-          
-          
+          console.log(
+            `${this.name} spider finished crawling check for data in relevant files.`
+          );
         } else {
-          // console.log(await value);
           this.data.push(await value);
         }
       }
     } catch (error) {
       console.log(error);
-    }finally{
-      this.browser.close()
+    } finally {
+      this.browser.close();
     }
   }
 
@@ -79,6 +78,18 @@ export class HealthEstablishmentsSpider {
    */
 
   async privinceDistrictsData(optionData) {
+    console.log("Getting districts data of respective province.");
+    // const closeButton = await this.page.$(
+    //   'button.btn.btn-primary[data-dismiss="modal"]'
+    // );
+    // const isButtonVisible = await closeButton.isIntersectingViewport();
+
+    // if (isButtonVisible) {
+    //   await closeButton.click();
+    // } else {
+    //   console.log("Button is not visible.");
+    // }
+
     const selectElement = await this.page.$("#province");
 
     await selectElement.select(optionData.value);
@@ -92,14 +103,119 @@ export class HealthEstablishmentsSpider {
         }))
         .filter((data) => !["0", "-1"].includes(data.value))
     );
-    // console.log(optionData.text);
-    // console.log("Selected option: ", districtData);
+
+    const districtIterator = this.healthCareFacilitiesGenerator(districtData);
+
+    let districtFacilities = {
+      numberOfHealthFacilities: 0,
+      primaryHealthFacilities: [],
+    };
+
+    for (let i = 0; i < districtData.length; i++) {
+      let { done, value } = districtIterator.next();
+      console.log(await value);
+      if (done) {
+        let dataList = await value;
+        districtFacilities.primaryHealthFacilities = dataList;
+        districtFacilities.numberOfHealthFacilities = dataList?.length;
+      }
+    }
+console.log(districtFacilities)
     return {
       province: optionData.text,
       districts: {
         total: districtData.length,
         names: districtData.map((data) => data.text),
+        districtFacilities,
       },
     };
+  }
+
+  /**
+   * @description scrapes Primary Health Care facilities data in respective district.
+   */
+
+  async healthCareFacilities(district) {
+    console.log("Getting health care facilities data of respective district.");
+
+    const selectElement = await this.page.$("#district");
+
+    await selectElement.select(district.value);
+    // Wait for the effects to take place
+    await this.page.waitForTimeout(10000); // Adjust the timeout as needed
+    // =====
+    const [button] = await this.page.$x('//*[@id="btnLoadMap"]');
+    if (button) {
+      button.click();
+      await this.page.waitForTimeout(10000);
+
+      const images = await this.page.$$(
+        "img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive"
+      );
+
+      const distrctFacilities = [];
+
+      for (const image of images) {
+        await image.click();
+        await this.page.waitForTimeout(10000);
+
+        const facilityData = await this.facilityDetails();
+
+        console.log(facilityData);
+        distrctFacilities.push(facilityData);
+        const closeButton = await this.page.$(
+          'button.btn.btn-primary[data-dismiss="modal"]'
+        );
+        await closeButton.click();
+        await this.page.waitForTimeout(10000);
+      }
+      return distrctFacilities;
+    }
+    // ====
+  }
+  *healthCareFacilitiesGenerator(districtOptions) {
+    for (let districtOption of districtOptions) {
+      yield this.healthCareFacilities(districtOption);
+    }
+  }
+  async facilityDetails() {
+    console.log("Getting facility information");
+    const facilityInfo = {};
+
+    try {
+      const element = await this.page.$("#facilityAccordion");
+      const cardElements = await element.$$(".card");
+
+      for (let cardElement of cardElements) {
+        const cardHeader = await cardElement.$(".card-header");
+        const headingText = await cardHeader.$$eval(
+          ".accordionHeading",
+          (elements) => elements[0].textContent.trim()
+        );
+        const cardBody = await cardElement.$(".card-body");
+        const tableRows = await cardBody.$$("tbody tr");
+        const rowData = {};
+
+        for (let row of tableRows) {
+          const cells = await row.$$("th, td");
+        
+          if (cells.length === 2) {
+            const [keyCell, valueCell] = cells;
+            const key = await keyCell.evaluate((element) => element.textContent.trim());
+            const value = await valueCell.evaluate((element) => element.textContent.trim());
+        
+            rowData[key] = value;
+          }
+        }
+        
+
+        facilityInfo[headingText] = rowData;
+      }
+
+      
+      return facilityInfo;
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
