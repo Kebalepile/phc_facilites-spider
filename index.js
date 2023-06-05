9add721f4047e54af07a1f6daa660458328359f5
Kebalepile <pimpabutterfly86@gmail.com>
2023-06-05 08:55:31 +0200

    added pipeline and database for healthEstablishments spider.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import { HealthEstablishmentsSpider } from "./spiders/healthEstablishments.js";
 async function crawler() {
   const browser = await launch({ headless: false });
 
-  const spider = new HealthEstablishmentsSpider(browser);
-  await spider.provincesData();
+  const HESpider = new HealthEstablishmentsSpider(browser);
+  await HESpider.startSpider();
 }
 
 crawler();
--- spiders/healthEstablishments.js
+++ spiders/healthEstablishments.js
@@ -1,90 +1,105 @@
+import { toJson} from "../pipline/healthEstablishmentPipline.js";
+
 
 export class HealthEstablishmentsSpider {
-    constructor(browser) {
-      this.page = null;
-      this.browser = browser;
-      this.allowedDomains = [
-        "https://www.healthestablishments.org.za/Home/Facility",
-      ];
-      this.data = [];
-    }
-    async setPage() {
-      this.page = await this.browser.newPage();
-    }
-  
-    /**
-     * @description scrape all the province's relative names.
-     */
-  
-    async provincesData() {
-      try {
-        if (!this.page) {
-          await this.setPage();
-        }
-        await this.page.goto(this.allowedDomains[0]);
-        await this.page.waitForSelector("body");
-        let optionValues = await this.page.$$eval("#province option", (options) =>
-          options
-            .map((option) => ({ value: option.value, text: option.textContent }))
-            .filter((data) => data.value !== "0")
-        );
-  
-        let iterator = this.destrictDataGenerator(optionValues);
-        for (let i = 0; i <= optionValues.length; i++) {
-          let { done, value } = iterator.next();
-          if (done) {
-            console.log("done");
-            console.log(this.data);
-          } else {
-            console.log(await value);
-            this.data.push(await value);
-          }
-        }
-      } catch (error) {
-        console.log(error);
+  constructor(browser) {
+    this.name = "Health Establlishment"
+    this.page = null;
+    this.browser = browser;
+    this.allowedDomains = [
+      "https://www.healthestablishments.org.za/Home/Facility",
+    ];
+    this.data = [];
+  }
+  async startSpider() {
+    try {
+      if (!this.page) {
+        await this.setPage();
       }
-    }
-  
-    /**
-     * @param {array} optionValues
-     * @description Iterators over array of optionValue objects.
-     */
-  
-    *destrictDataGenerator(optionValues) {
-      for (let optionValue of optionValues) {
-        yield this.privinceDistrictsData(optionValue);
-      }
-    }
-  
-    /**
-     *
-     * @param {object} optionData
-     * @returns province data object with province name and districts found in province.
-     */
-  
-    async privinceDistrictsData(optionData) {
-      const selectElement = await this.page.$("#province");
-  
-      await selectElement.select(optionData.value);
-      // Wait for the effects to take place
-      await this.page.waitForTimeout(10000); // Adjust the timeout as needed
-      const districtData = await this.page.$$eval("#district option", (options) =>
-        options
-          .map((option) => ({
-            value: option.value,
-            text: option.textContent,
-          }))
-          .filter((data) => !["0", "-1"].includes(data.value))
-      );
-      // console.log(optionData.text);
-      // console.log("Selected option: ", districtData);
-      return {
-        province: optionData.text,
-        districts: {
-          total: districtData.length,
-          names: districtData.map((data) => data.text),
-        },
-      };
+      console.log(`${this.name} spider, has began crawling for data.`)
+      this.provincesData();
+    } catch (error) {
+      consle.log(error);
     }
   }
-  
+  async setPage() {
+    this.page = await this.browser.newPage();
+  }
+
+  /**
+   * @description scrape all the province's relative names.
+   */
+
+  async provincesData() {
+    try {
+      await this.page.goto(this.allowedDomains[0]);
+      await this.page.waitForSelector("body");
+      let optionValues = await this.page.$$eval("#province option", (options) =>
+        options
+          .map((option) => ({ value: option.value, text: option.textContent }))
+          .filter((data) => data.value !== "0")
+      );
+
+      let iterator = this.destrictDataGenerator(optionValues);
+      for (let i = 0; i <= optionValues.length; i++) {
+        let { done, value } = iterator.next();
+        if (done) {
+          console.log("done");
+          // console.log(this.data);
+          toJson(this.data);
+          
+          
+        } else {
+          // console.log(await value);
+          this.data.push(await value);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }finally{
+      this.browser.close()
+    }
+  }
+
+  /**
+   * @param {array} optionValues
+   * @description Iterators over array of optionValue objects.
+   */
+
+  *destrictDataGenerator(optionValues) {
+    for (let optionValue of optionValues) {
+      yield this.privinceDistrictsData(optionValue);
+    }
+  }
+
+  /**
+   *
+   * @param {object} optionData
+   * @returns province data object with province name and districts found in province.
+   */
+
+  async privinceDistrictsData(optionData) {
+    const selectElement = await this.page.$("#province");
+
+    await selectElement.select(optionData.value);
+    // Wait for the effects to take place
+    await this.page.waitForTimeout(10000); // Adjust the timeout as needed
+    const districtData = await this.page.$$eval("#district option", (options) =>
+      options
+        .map((option) => ({
+          value: option.value,
+          text: option.textContent,
+        }))
+        .filter((data) => !["0", "-1"].includes(data.value))
+    );
+    // console.log(optionData.text);
+    // console.log("Selected option: ", districtData);
+    return {
+      province: optionData.text,
+      districts: {
+        total: districtData.length,
+        names: districtData.map((data) => data.text),
+      },
+    };
+  }
+}
