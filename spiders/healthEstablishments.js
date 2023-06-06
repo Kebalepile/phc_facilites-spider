50f603e07599c537c8839f78ebe40ec6bf055b76
Kebalepile <pimpabutterfly86@gmail.com>
2023-06-06 15:28:13 +0200

    still struggling with timeouts when dealing with buttons

--- spiders/healthEstablishments.js
+++ spiders/healthEstablishments.js
@@ -16,21 +16,19 @@ export class HealthEstablishmentsSpider {
         await this.setPage();
       }
       console.log(`${this.name} spider, has began crawling for data.`);
-      
+
       await this.provincesData();
 
-      if(this.data.length){
-     
-          toJson(this.data);
-          console.log(
-            `${this.name} spider finished crawling check for data in relevant files.`
-          );
-          return;
-        
+      if (this.data.length) {
+        toJson(this.data);
+        console.log(
+          `${this.name} spider finished crawling check for data in relevant files.`
+        );
+        return;
       }
-      console.log("No data scraped !!!")
+      console.log("No data scraped !!!");
     } catch (error) {
-      consle.log(error);
+      console.log(error);
     }
   }
   async setPage() {
@@ -43,29 +41,31 @@ export class HealthEstablishmentsSpider {
 
   async provincesData() {
     try {
-     
-
       await this.page.goto(this.allowedDomains[0]);
       await this.page.waitForSelector("body");
-      let provinceOptions = await this.page.$$eval("#province option", (options) =>
-        options
-          .map((option) => ({ value: option.value, text: option.textContent }))
-          .filter((data) => data.value !== "0")
+      let provinceOptions = await this.page.$$eval(
+        "#province option",
+        (options) =>
+          options
+            .map((option) => ({
+              value: option.value,
+              text: option.textContent,
+            }))
+            .filter((data) => data.value !== "0")
       );
 
-     
-      for (let provinceOption of provinceOptions){
-        let districtInfo = await this.privinceDistrictsData(provinceOption);
-        this.data.push( districtInfo)
+      for (let provinceOption of provinceOptions) {
+        let completeProvinceData = await this.privinceDistrictsData(
+          provinceOption
+        );
+        this.data.push(completeProvinceData);
       }
-      
     } catch (error) {
       console.log(error);
     } finally {
       this.browser.close();
     }
   }
-
 
   /**
    *
@@ -78,7 +78,7 @@ export class HealthEstablishmentsSpider {
       let selectElement = await this.page.$("#province");
 
       await selectElement.select(option.value);
-      await this.page.waitForTimeout(5000); 
+      await this.page.waitForTimeout(5000);
 
       let districtOptions = await this.page.$$eval(
         "#district option",
@@ -91,26 +91,29 @@ export class HealthEstablishmentsSpider {
             .filter((data) => !["0", "-1"].includes(data.value))
       );
 
-      let districtFacilities = {
-        numberOfHealthFacilities: 0,
-        healthFacilities: [],
-      };
-
-      for (let districtOption of districtOptions) {
-        let districtHealthFacilites = await this.healthCareFacilities(districtOption);
-
-        districtFacilities.healthFacilities = districtHealthFacilites;
-        districtFacilities.numberOfHealthFacilities = districtHealthFacilites?.length;
-      }
-      console.log(districtFacilities);
-      return {
+      let completeProvinceData = {
         province: option.text,
         districts: {
-          total: districtOptions.length,
-          names: districtOptions.map((data) => data.text),
-          districtFacilities,
+          totalDistricts: districtOptions.length,
+          districtNames: districtOptions.map((data) => data.text),
+          districtFacilities: {
+            numberOfHealthFacilites: 0,
+            healthFacilities: [],
+          },
         },
       };
+      for (let districtOption of districtOptions) {
+        let districtHealthFacilities = await this.healthCareFacilities(
+          districtOption
+        );
+        completeProvinceData.districts.districtFacilities.healthFacilities.concat(
+          districtHealthFacilities
+        );
+        completeProvinceData.districts.districtFacilities.numberOfHealthFacilites =
+          completeProvinceData.districts.districtFacilities.healthFacilities.length;
+      }
+      console.log(completeProvinceData);
+      return completeProvinceData;
     } catch (error) {
       console.log(error);
     }
@@ -121,6 +124,7 @@ export class HealthEstablishmentsSpider {
    */
 
   async healthCareFacilities(district) {
+    let districtHealthFacilities = [];
     try {
       let selectElement = await this.page.$("#district");
 
@@ -137,32 +141,29 @@ export class HealthEstablishmentsSpider {
           "img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive"
         );
 
-        let districtFacilities = [];
-
         for (let image of images) {
-          await this.page.waitForTimeout(6000);
+     
           await image.click();
-
-          await this.page.waitForTimeout(6000);
+          await this.page.waitForTimeout(5000);
+         
 
           let facilityData = await this.facilityDetails();
 
           console.log(facilityData);
 
-          districtFacilities.push(facilityData);
+          districtHealthFacilities.push(facilityData);
 
-          let closeButton = await this.page.$(
-            'button.btn.btn-primary[data-dismiss="modal"]'
-          );
-          await this.page.waitForTimeout(6000);
+          
+          // Click the close button inside the modal
+          let closeButton = await this.page.$(".modal.fade.in button.close");
           if (closeButton) {
-            
+            await this.page.waitForTimeout(10000)
             await closeButton.click();
-            await this.page.waitForTimeout(6000);
           }
         }
-        return districtFacilities;
+        
       }
+      return districtHealthFacilities;
     } catch (error) {
       console.log(error);
     }
